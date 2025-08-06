4fd015febadcfc43bbe4e2b3d4f4a8fe9623f68a
Reebix <timon@set-richter.de>
2025-08-06 16:41:26 +0200

    changed remote path

--- 7z/sevenzip_utils.py
+++ 7z/sevenzip_utils.py
@@ -6,7 +6,7 @@ import shutil
 
 def copy_from_network_drive():
     """Download 7zip from network drive"""
-    source = r"\\Labs\labs\01_Installers_Utilities\7ZIP\7zr_25.00.exe"
+    source = r"\\arthas\Media\benchmarks\7ZIP\7zr_25.00.exe"
     root_dir = os.path.dirname(os.path.realpath(__file__))
     destination = os.path.join(root_dir, "7zr_25.00.exe")
     shutil.copyfile(source, destination)
